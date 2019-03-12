54819f886e1a4aef4fab36b11b262daaa45596b4
a8568730 <a8568730@hotmail.com>
2019-03-12 16:37:57 +0800

    * liânKù 包彼兩个函式

--- tauphahji_cmd.py
+++ tauphahji_cmd.py
@@ -24,8 +24,16 @@ def tàuphahjī(漢羅):
     return json.loads(responseStr)
 
 
+def liânKù(多元書寫, 欲連的key):
+    if 欲連的key == '漢字':
+        return liânHànjī(多元書寫)
+    elif 欲連的key == '臺羅':
+        return liânTâilô(多元書寫)
+
+
 def liânHànjī(多元書寫):
-    return ''.join([i['漢字'].replace('.','。') for i in 多元書寫])
+    return ''.join([i['漢字'].replace('.', '。') for i in 多元書寫])
+
 
 def liânTâilô(多元書寫):
     return '. '.join([i['臺羅'] for i in 多元書寫])
